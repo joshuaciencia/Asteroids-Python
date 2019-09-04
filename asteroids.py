6c11f462d13c6c62a842190574c177c49ded8d19
Joshuaciencia <joshuaciencia2016@gmail.com>
2019-09-04 00:52:04 -0500

    improved player motion

--- asteroids.py
+++ asteroids.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from pygame.locals import *
+from pygame.math import Vector2
 # initializa pygame library
 pygame.init()
 # window dimensions
@@ -12,16 +13,18 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 # player properties (position, size, speed)
-playerX = WIDTH / 2
-playerY = HEIGHT / 2
-playerSize = 20
-playerSpeed = 4
+p_x = WIDTH / 2
+p_y = HEIGHT / 2
+p_width = 12
+p_height = 20
+p_speed = 4
 thickness = 2;
-#font property
-fontObj = pygame.font.Font('freesansbold.ttf', 32)
-textSurfaceObj = fontObj.render('Holberton Game !', True, GREEN, BLUE)
-textRectObj = textSurfaceObj.get_rect()
-textRectObj.center = (WIDTH / 2, (HEIGHT / 2) - 100)
+angle = -90
+velocity = Vector2()
+heading = Vector2(0, -1)
+left = Vector2()
+right = Vector2()
+front = Vector2()
 # the game will run at 30 frames per second
 FPS = 30
 # this obj is used ti run the game at the desired speed (FPS)
@@ -32,32 +35,47 @@ window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Holbi Game')
 # main game loop
 while True:
-    # fill the window with black color
-    window.fill(BLACK)
-    # draw the text obj
-    window.blit(textSurfaceObj, textRectObj)
+
+    if pygame.key.get_pressed()[K_UP]:
+        tmp = Vector2(heading)
+        tmp.scale_to_length(p_speed)
+        p_x += -tmp.x
+        p_y += tmp.y
+    if pygame.key.get_pressed()[K_LEFT]:
+        heading = heading.rotate(10)
+    if pygame.key.get_pressed()[K_RIGHT]:
+        heading = heading.rotate(-10)
+
     # draw player ship
+    left.x = - p_width / 2
+    left.y = p_height / 2
+
+    right.x = p_width / 2
+    right.y = p_height / 2
+
+    front.x = 0
+    front.y = - p_height / 2
+
+    angle = heading.angle_to(Vector2(0, -1))
+    left = left.rotate(angle)
+    right = right.rotate(angle)
+    front = front.rotate(angle)
+
+    # fill window with black (clear)
+    window.fill(BLACK)
+
     # draw left side
-    pygame.draw.line(window, WHITE,
-            (playerX - playerSize / 2, playerY + playerSize / 2),
-            (playerX, playerY - playerSize / 2), thickness)
+    pygame.draw.line(window, WHITE, 
+            (p_x + left.x, p_y + left.y),
+            (p_x + front.x, p_y + front.y), thickness)
     # draw right side
-    pygame.draw.line(window, WHITE,
-            (playerX, playerY - playerSize / 2),
-            (playerX + playerSize / 2, playerY + playerSize / 2), thickness)
+    pygame.draw.line(window, WHITE, 
+            (p_x + front.x, p_y + front.y), 
+            (p_x + right.x, p_y + right.y), thickness)
     # draw back side
     pygame.draw.line(window, WHITE, 
-            (playerX - playerSize / 2, playerY + playerSize / 2),
-            (playerX + playerSize / 2, playerY + playerSize / 2), thickness)
-    # get player input. Note: this is not how the ship should move, this is just test code
-    if pygame.key.get_pressed()[K_UP]:
-        playerY -= playerSpeed
-    if pygame.key.get_pressed()[K_DOWN]:
-        playerY += playerSpeed
-    if pygame.key.get_pressed()[K_LEFT]:
-        playerX -= playerSpeed
-    if pygame.key.get_pressed()[K_RIGHT]:
-        playerX += playerSpeed
+            (p_x + right.x, p_y + right.y),
+            (p_x + left.x, p_y + left.y), thickness)
     # handle events
     for event in pygame.event.get():
         if event.type == QUIT:
